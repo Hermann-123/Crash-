db1c0d1b3d228dd78b23542a2fadc132b5012dbd
Hermann-123 <degbegnonhermann096@gmail.com>
2026-08-08 09:13:22 +0000

    Update core.py

--- app/core.py
+++ app/core.py
@@ -5,7 +5,7 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(level
 logger = logging.getLogger("WallStreet_OS")
 
 class Settings:
-    TELEGRAM_BOT_TOKEN: str = os.getenv("TELEGRAM_BOT_TOKEN", "8000472746:AAFtFQ8O_gjmKiAC072fn5xvx0Jsf0cK5s0")
+    TELEGRAM_BOT_TOKEN: str = os.getenv("TELEGRAM_BOT_TOKEN", "8000472746:AAGm85smi44PE4VV-dyCn6eKgwC_K38vYuI")
     ADMIN_ID: int = int(os.getenv("ADMIN_ID", "5968288964"))
     GROQ_API_KEY: str = os.getenv("GROQ_API_KEY", "")
     
